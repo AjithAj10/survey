cfad878f64b1a84ff402eb2ccc4b08ade0ff2096
ajith <ajithraveendranr@gmail.com>
2022-07-23 15:53:33 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Create from './Components/Create';
 
 function App() {
+  const [create,setCreate] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tiger Survey</h1>
+      <button onClick={() => setCreate('ok')}>Create Survey</button>
+      {
+        create && <Create/>
+      }
+      <button>Take Survey</button>
     </div>
   );
 }
